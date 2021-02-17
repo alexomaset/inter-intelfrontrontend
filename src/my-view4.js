30a5c0ee83578f5d95f9ec380fc97e90580b1301
Alex Omaset <alexomaset711@gmail.com>
2021-02-17 10:54:04 +0300

    add initial changes

--- src/my-view4.js
+++ src/my-view4.js
@@ -52,11 +52,7 @@ class MyView4 extends PolymerElement {
         <div class="circle">2</div>
         <h1>View Two</h1>
         <p>Ea duis bonorum nec, falli paulo aliquid ei eum.</p>
-        <p>
-          Id nam odio natum malorum, tibique copiosae expetenda mel ea.Detracto
-          suavitate repudiandae no eum. Id adhuc minim soluta nam.Id nam odio
-          natum malorum, tibique copiosae expetenda mel ea.
-        </p>
+       
       </div>
     `;
   }
